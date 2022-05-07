d288ede93c17fda4d021bb627d40b172521b12d4
Mustafa ahmed <66968111+MustafaAhmed22@users.noreply.github.com>
2022-05-07 22:40:56 +0200

    a

--- Rock-paper-game/script.js
+++ Rock-paper-game/script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const computerChoiece=  document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+       userChoice = e.target.id   
+       userChoiceDisplay.innerHTML=userChoice
+       generateComputerChoice()
+}))
+function generateComputerChoice() {
+      const randomnumber = Math.floor(Math.random() * possibleChoices.length) //3
+      console.log(randomnumber)
+
+}
