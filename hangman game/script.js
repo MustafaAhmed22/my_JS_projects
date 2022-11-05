f17f16a45620ae2be09f70a03f0125e5115f65fe
Mustafa ahmed <66968111+MustafaAhmed22@users.noreply.github.com>
2022-11-05 16:31:20 +0200

    h1

---
+++ hangman game/script.js
@@ -0,0 +1,31 @@
+const prompt = require("prompt-sync")({
+    sigint: true
+});
+let hangmanArr =['table','apple','city','computer','window']
+var randomItem = hangmanArr[Math.floor(Math.random() * hangmanArr.length)];
+let guessWordToArray =randomItem.split('')
+console.log(guessWordToArray)
+let shadowWordArray =[]
+for(let j=0;j<guessWordToArray.length;j++){
+    shadowWordArray.push('_')
+}
+console.log(shadowWordArray)
+    let countLifes=5
+for(let i=0;i<shadowWordArray.length;i++){
+const char = prompt("enter char : ");
+if (guessWordToArray.includes(char)){
+    let ind = guessWordToArray.indexOf(char)
+    shadowWordArray.splice(ind,1,char)
+    console.log(shadowWordArray)
+
+}else{
+    countLifes--
+    if(countLifes === 0){
+        console.log(countLifes)
+        console.log('you failed!')
+        break
+    }
+}
+}
+
+
