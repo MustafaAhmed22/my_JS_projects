db810069cfd06f5e287b6486c07d0968152b7d53
Mustafa ahmed <66968111+MustafaAhmed22@users.noreply.github.com>
2022-12-06 00:09:22 +0200

    design 3

--- ToDo APP/script.js
+++ ToDo APP/script.js
@@ -10,27 +10,58 @@ let todo =[
     },
     {
            text: 'watch'
-    },
-    {
-            text: 'read'
     }
 ]
-
+//render all todos
 todo.map(function(t){
     let createtodo = document.createElement('div')
-    createtodo.textContent = t.text
+    let createSpan =document.createElement('span')
+    createSpan.className='span'
+    createSpan.textContent = t.text
     createtodo.className = "todo"
     document.querySelector('.todo_container').appendChild(createtodo)
-    
-    let todobtns =document.querySelector('div')
-    todobtns.className = 'done'
-    let btn1 =document.querySelector('button')
-    let btn2 = document.querySelector('button')
-    btn1.className = 'done'
-    btn2.className = 'del'
-    document.querySelector('.done').appendChild(btn1)
-    document.querySelector('.done').appendChild(btn2)
+    createtodo.appendChild(createSpan)
+    let createBtn1=document.createElement('button')
+    createBtn1.textContent ='DONE'
+    createBtn1.className='b done'
+    let createBtn2 = document.createElement('button')
+    createBtn2.textContent = 'DELETE'
+    createBtn1.className = 'b delete'
+        createtodo.append(createBtn1)
+        createtodo.append(createBtn2)
+})
+//remove all todos
+document.querySelector('.remove_all').addEventListener('click',function(e){
+   todo.map(function(r){
+    document.querySelector('.todo').remove(e)
+   })
+})
+//add new todo
+document.querySelector('.add').addEventListener('click',function(e){
+    let createtodo = document.createElement('div')
+    let createSpan = document.createElement('span')
+    let input = document.querySelector('.inp').value
+    if(input === ''){
+        alert('انتا عبيط!')
+    }else{
+createSpan.className = 'span'
+createSpan.textContent = input
+createtodo.className = "todo"
+document.querySelector('.todo_container').appendChild(createtodo)
+createtodo.appendChild(createSpan)
+let createBtn1 = document.createElement('button')
+createBtn1.textContent = 'DONE'
+createBtn1.className = 'b'
+let createBtn2 = document.createElement('button')
+createBtn2.textContent = 'DELETE'
+createBtn1.className = 'b'
+createtodo.append(createBtn1)
+createtodo.append(createBtn2)
 
-    document.querySelector('.todo_container').appendChild(todobtns)
+}
     
+})
+// done
+document.querySelector('.done').addEventListener('click',function(e){
+    document.querySelector()
 })
