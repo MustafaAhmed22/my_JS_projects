27abc0d4ca6f96acfa36e17520131c8a644c38fa
Mustafa ahmed <66968111+MustafaAhmed22@users.noreply.github.com>
2022-12-06 20:07:21 +0200

    javascript ended

--- Rock-paper-game/script.js
+++ Rock-paper-game/script.js
@@ -1,15 +1,48 @@
-const computerChoiece=  document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let userChoice
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-       userChoice = e.target.id   
-       userChoiceDisplay.innerHTML=userChoice
-       generateComputerChoice()
-}))
-function generateComputerChoice() {
-      const randomnumber = Math.floor(Math.random() * possibleChoices.length) //3
-      console.log(randomnumber)
+//console.log(randomItem)
 
+const computerChoiece = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('User-choice')
+const resultDisplay = document.getElementById('result')
+let paperbtn =document.querySelector('#paper')
+let rockbtn = document.querySelector('#rock')
+let scissorsbtn = document.querySelector('#scissors')
+
+paperbtn.addEventListener('click',function(e){
+      console.log(e.target.innerText)
+    userChoiceDisplayfun(e)
+           
+})
+rockbtn.addEventListener('click', function (e) {
+      console.log(e.target.innerText)
+      userChoiceDisplayfun(e)
+})
+scissorsbtn.addEventListener('click', function (e) {
+      console.log(e.target.innerText)
+      userChoiceDisplayfun(e)
+})
+
+function userChoiceDisplayfun(e){
+ userChoiceDisplay.textContent=e.target.innerText
+ let choice = ['Rock', 'Paper', 'Scissors']
+ var randomItem = choice[Math.floor(Math.random() * choice.length)];
+ computerChoiece.textContent = randomItem
+ resultfun()
+}
+
+function resultfun(){
+      if (userChoiceDisplay.textContent === computerChoiece.textContent) {
+            resultDisplay.textContent ='duel'
+      } else if (userChoiceDisplay.textContent === 'Paper' && computerChoiece.textContent === 'Rock') {
+            resultDisplay.textContent='you win'
+      } else if (userChoiceDisplay.textContent === 'Paper' && computerChoiece.textContent === 'Scissors') {
+            resultDisplay.textContent ='computer wins'
+      } else if (userChoiceDisplay.textContent === 'Rock' && computerChoiece.textContent === 'Scissors') {
+            resultDisplay.textContent = 'you win'
+      } else if (userChoiceDisplay.textContent === 'Rock' && computerChoiece.textContent === 'Paper') {
+            resultDisplay.textContent = 'computer wins'
+      } else if (userChoiceDisplay.textContent === 'Scissors' && computerChoiece.textContent === 'Rock') {
+            resultDisplay.textContent = 'computer wins'
+      } else if (userChoiceDisplay.textContent === 'Scissors' && computerChoiece.textContent === 'Paper') {
+            resultDisplay.textContent = 'you win'
+      }
 }
