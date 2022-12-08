6a44bcb9a44c977bcead82eb75f5225b0a5ee017
Mustafa ahmed <66968111+MustafaAhmed22@users.noreply.github.com>
2022-12-08 06:50:38 +0200

    calculator final

---
+++ calculator/script.js
@@ -0,0 +1,23 @@
+let btn =document.querySelectorAll('.btn')
+let screen = document.querySelector('.screen')
+//console.log(btn[0])
+//let result =[]
+let str =''
+btn.forEach(function(b){
+    b.addEventListener('click',function(e){
+        //console.log(e.target.textContent)
+        //result.push(e.target.textContent)
+        //let arr=result.join('')
+        if (str.startsWith('*') || str.startsWith('+') || str.startsWith('-' || str.startsWith('/'))){
+            str =''
+        }
+        if (e.target.textContent === '=') {
+            screen.textContent = eval(str)
+            str = ''
+        }else{
+            str += e.target.textContent
+            console.log(str)
+            screen.textContent = str
+        }
+    })
+})
