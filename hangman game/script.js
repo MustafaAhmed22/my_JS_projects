c0def6670b0b0f2a00626ecca3a432c93b6fb898
Mustafa ahmed <66968111+MustafaAhmed22@users.noreply.github.com>
2022-12-20 23:09:43 +0200

    hangman final

--- hangman game/script.js
+++ hangman game/script.js
@@ -1,67 +1,70 @@
-/*
-const prompt = require("prompt-sync")({
-    sigint: true
-});
-let hangmanArr =['table','apple','city','computer','window']
-var randomItem = hangmanArr[Math.floor(Math.random() * hangmanArr.length)];
-let guessWordToArray =randomItem.split('')
-console.log(guessWordToArray)
-let shadowWordArray =[]
-for(let j=0;j<guessWordToArray.length;j++){
-    shadowWordArray.push('_')
-}
-console.log(shadowWordArray)
-    let countLifes=5
-for(let i=0;i<shadowWordArray.length;i++){
-const char = prompt("enter char : ");
-if (guessWordToArray.includes(char)){
-    let ind = guessWordToArray.indexOf(char)
-    shadowWordArray.splice(ind,1,char)
-    console.log(shadowWordArray)
-
-}else{
-    countLifes--
-    if(countLifes === 0){
-        console.log(countLifes)
-        console.log('you failed!')
-        break
-    }
-}
-}
-*/
-
-let words = ['table', 'apple', 'city', 'computer', 'window']
+let counter =0
+let imgArray = [
+    'url("img/1.jpg")',
+    'url("img/2.jpg")',
+    'url("img/3.jpg")',
+    'url("img/4.jpg")',
+    'url("img/5.jpg")',
+    'url("img/6.jpg")',
+    'url("img/7.jpg")'
+ ]
+ let loadImage = document.querySelector('.imgs-container')
+let words = ['table', 'apple', 'city', 'computer', 'window','cat','lion','elephant','sofa','cupboard','chair']
 var randomWord = words[Math.floor(Math.random() * words.length)];
 console.log(randomWord)
 let randomWordToArray =randomWord.split('')
-
-randomWordToArray.map(function (e) {
-    let createSpan =document.createElement('span')
-    createSpan.className ='span-css'
-    //createSpan.textContent =e
-    document.querySelector('.letters').append(createSpan)
-})
+let shadowArray =[]
+for (let j = 0; j < randomWordToArray.length; j++) {
+    shadowArray.push(' ')
+}
+console.log(shadowArray)
 let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 console.log(letters.length)
-
 letters.map(function(letter){
     let createBtn =document.createElement('button')
     createBtn.className ='btn'
     createBtn.textContent =letter
     document.querySelector('.btns-container').append(createBtn)
 })
-
-let btn =document.querySelectorAll('.btn')
-btn.forEach(function(b){
-    b.addEventListener('click',function(e){
+ let createSpan 
+shadowArray.map(function (e) {
+    createSpan = document.createElement('span')
+    createSpan.className = 'span-css'
+    createSpan.textContent = e
+    //createSpan.style.color = 'white'
+    document.querySelector('.letters').append(createSpan)
+})
+let btn = document.querySelectorAll('.btn')
+btn.forEach(function (b) {
+    b.addEventListener('click', function (e) {
         console.log(e.target.textContent)
-        b.disabled =true
-        b.className ='disabled'
-        if (randomWordToArray.includes(e.target.textContent)){
-            let ind = randomWordToArray.indexOf(e.target.textContent)
-            createSpan.textContent =randomWordToArray[ind]
-
-            
+        b.disabled = true
+        b.className = 'disabled'
+         if (randomWordToArray.includes(e.target.textContent)) {
+            for(let j=0;j<randomWordToArray.length;j++){
+                    if (randomWordToArray[j] === e.target.textContent){
+                    let ind = randomWordToArray.indexOf(e.target.textContent)
+                    shadowArray.splice(ind, 1, e.target.textContent)
+                //shadowArray[ind] = e.target.textContent
+                document.querySelector('.letters').children[ind].
+                replaceChild(document.createTextNode(e.target.textContent),
+                 document.querySelector('.letters').children[ind].childNodes[0])
+                 randomWordToArray.splice(ind, 1, '*')
+                }
+            }
+        }else{
+            if (counter !== 7) {
+               loadImage.style.backgroundImage = imgArray[counter]
+               counter++
+            } else {
+                alert('Failed , Play Again')
+                 window.location.reload();
+            }
+        }
+        if(!shadowArray.includes(' ')){
+            alert('you win, Play Again')
+             window.location.reload();
         }
     })
+   
 })
--- quote generator/script.js
+++ quote generator/script.js
@@ -22,13 +22,13 @@
                     var randomItem = quotes[Math.floor(Math.random() * quotes.length)];
                     //console.log(randomItem)
                     let loadimg = [
-                       'url("1.jpg")',
-                        'url("2.jpg")',
-                        'url("3.jpg")',
-                        'url("4.jpg")',
-                        'url("5.jpg")',
-                        'url("6.jpg")',
-                        'url("7.jpg")'
+                       'url("img/1.jpg")',
+                        'url("img/2.jpg")',
+                        'url("img/3.jpg")',
+                        'url("img/4.jpg")',
+                        'url("img/5.jpg")',
+                        'url("img/6.jpg")',
+                        'url("img/7.jpg")'
                     ]
                     /*
                     loadimg[1] = '\2.jpg',
